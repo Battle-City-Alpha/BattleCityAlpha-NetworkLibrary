590073fd4649e47923b51dee495c36cbf6792e85
Tic-Tac-Toc <TicTacTocProd@gmail.com>
2019-07-30 23:00:24 +0200

    Update rank

--- Network/Packets/Enums/PlayerRank.cs
+++ Network/Packets/Enums/PlayerRank.cs
@@ -8,12 +8,12 @@ namespace BCA.Network.Packets.Enums
 {
     public enum PlayerRank
     {
-        Player = 0,
-        Ruller = 1,
-        Animator = 2,
-        Developper = 3,
-        Moderator = 4,
-        Bot = 98,
-        Owner = 99
+        Joueurs = 0,
+        Rullers = 1,
+        Animateurs = 2,
+        Developpeurs = 3,
+        Moderateurs = 4,
+        Robots = 98,
+        Administrateurs = 99
     }
 }
