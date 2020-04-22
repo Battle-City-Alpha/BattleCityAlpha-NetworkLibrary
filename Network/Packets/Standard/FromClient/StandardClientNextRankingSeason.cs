c207d06e9d06a8b0f35a4dd2bd8f4967df3cc180
Louis Caldas <louis.caldas@telecom-sudparis.eu>
2020-04-22 19:45:58 +0200

    Clean code & support win7

--- Network/Packets/Standard/FromClient/StandardClientNextRankingSeason.cs
+++ Network/Packets/Standard/FromClient/StandardClientNextRankingSeason.cs
@@ -1,4 +1,10 @@
-﻿namespace BCA.Network.Packets.Standard.FromClient
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCA.Network.Packets.Standard.FromClient
 {
     public class StandardClientNextRankingSeason : Packet
     {
